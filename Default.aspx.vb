0d2b9cfe97c56f8d7d235ca2de47d90514e1f107
Enio Fernandes <ef@cic.ac>
2017-06-15 22:06:40 +0000

    clean code

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,13 +1,18 @@
-﻿Imports HtmlAgilityPack
+Imports HtmlAgilityPack
 
 Partial Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim doc As HtmlDocument
-        doc = New HtmlWeb().Load("https://github.com/EN10/KerasInception")
 
-        Dim node = doc.DocumentNode.SelectNodes("//img").ElementAt(4)
+        Dim url = "https://github.com/EN10/KerasInception"
+        Dim doc As HtmlDocument = New HtmlWeb().Load(url)
+
+        Dim nodes = doc.DocumentNode.SelectNodes("//img")
+
+        Dim size = nodes.Count
+        Dim node = nodes.ElementAt(4)
+
         Dim src = node.GetAttributeValue("src", "")
         Label1.Text = src
 
